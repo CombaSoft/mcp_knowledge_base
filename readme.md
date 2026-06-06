f451b309d3d83786e2784636cbfca4aa7b79474c
Vladimir Sitnikov <Vladimir.Sitnikov@magenta-technology.ru>
2026-06-06 10:04:24 +0400

    Prepare for async support

---
+++ readme.md
@@ -0,0 +1,8 @@
+MCP based RAG
+
+Java 17. 
+Can`t use Java 25 cause of error outputs into console.
+
+Requirements: 
+- started Open AI API server with model mentioned in configuration 
+- started Docker with Qdrant 
--- src/main/java/com/combasoft/ai/mcp/kb/KbServiceApplication.java
+++ src/main/java/com/combasoft/ai/mcp/kb/KbServiceApplication.java
@@ -2,7 +2,9 @@ package com.combasoft.ai.mcp.kb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication()
 public class KbServiceApplication {
     public static void main(String[] args) {
